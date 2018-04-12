0f4d327b67cae1755ff4a3820b364b8c02a520e9
Andrei Tudor <andrei.tudor27@gmail.com>
2018-04-12 13:50:20 +0200

    Delete

--- web/objects/project.php
+++ web/objects/project.php
@@ -1,36 +1,40 @@
 <?php
-class Project{
+class Project
+{
 
-	private $conn;
-	private $table_name = "projects";
+    private $conn;
+    private $table_name = "projects";
 
-	// public $id;
-	// public $name;
-	// public $description;
-	// public $price;
-	// public $category_id;
-	// public $category_name;
-	// public $created;
+    // public $id;
+    // public $name;
+    // public $description;
+    // public $price;
+    // public $category_id;
+    // public $category_name;
+    // public $created;
 
-	public function __construct($db){
-		$this->conn = $db;
-	}
-	// read products
-function read(){
- 
-    // select all query
-    $query = "SELECT * FROM " . $this->table_name;
- 
-    // prepare query statement
-    $stmt = $this->conn->prepare($query);
- 
-    // execute query
-    $stmt->execute();
- 
-    return $stmt;
-}
-function create() {
-	$query = "INSERT INTO
+    public function __construct($db)
+    {
+        $this->conn = $db;
+    }
+    // read products
+    public function read()
+    {
+
+        // select all query
+        $query = "SELECT * FROM " . $this->table_name;
+
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+
+        // execute query
+        $stmt->execute();
+
+        return $stmt;
+    }
+    public function create()
+    {
+        $query = "INSERT INTO
 	" . $this->table_name . "
 	SET
 	address = :address,
@@ -43,31 +47,42 @@ function create() {
 	img = :img,
 	user_id = 0";
 
-	$stmt = $this->conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
-	//TODO Sanitization of special characters
+        //TODO Sanitization of special characters
 
-	//bind values
-	$tags = $this->tags;
-	$tags = implode(",", $tags);
-	$stmt->bindParam(':address',$this->address);
-	$stmt->bindParam(':title',$this->title);
-	$stmt->bindParam(':description',$this->description);
-	$stmt->bindParam(':open_date',$this->open_date);
-	$stmt->bindParam(':tags',$tags);
-	$stmt->bindParam(':progress',$this->progress);
-	$stmt->bindParam(':hits',$this->hits);
-	$stmt->bindParam(':img',$this->img);
+        //bind values
+        $tags = $this->tags;
+        $tags = implode(",", $tags);
+        $stmt->bindParam(':address', $this->address);
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':open_date', $this->open_date);
+        $stmt->bindParam(':tags', $tags);
+        $stmt->bindParam(':progress', $this->progress);
+        $stmt->bindParam(':hits', $this->hits);
+        $stmt->bindParam(':img', $this->img);
 
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
 
-	if($stmt->execute()){
-		return true;
-	}
-	return false;
+    }
+    public function delete()
+    {
+        $query = "DELETE FROM " . $this->table_name . " WHERE id= ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        if ($stmt->execute()) {
+            return true;
+        }
 
-}
-function update() {
-	$query = "UPDATE
+        return false;
+    }
+    public function update()
+    {
+        $query = "UPDATE
 			" . $this->table_name . "
 			SET
 			address = :address,
@@ -80,25 +95,24 @@ function update() {
 			img = :img,
 			WHERE
 				id = :id";
-	$stmt = $this->conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
-	//TODO Sanitization of special characters
+        //TODO Sanitization of special characters
 
-	//bind values
+        //bind values
 
-	$stmt->bindParam(':title',$this->title);
-	$stmt->bindParam(':address',$this->address);
-	$stmt->bindParam(':description',$this->description);
-	$stmt->bindParam(':open_date',$this->open_date);
-	$stmt->bindParam(':tags',$this->tags);
-	$stmt->bindParam(':progress',$this->progress);
-	$stmt->bindParam(':hits',$this->hits);
-	$stmt->bindParam(':img',$this->img);
+        $stmt->bindParam(':title', $this->title);
+        $stmt->bindParam(':address', $this->address);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':open_date', $this->open_date);
+        $stmt->bindParam(':tags', $this->tags);
+        $stmt->bindParam(':progress', $this->progress);
+        $stmt->bindParam(':hits', $this->hits);
+        $stmt->bindParam(':img', $this->img);
 
-		if($stmt->execute()){
-			return true;
-		}
-		return false;
+        if ($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
 }
-}
-?>
--- web/project/delete.php
+++ web/project/delete.php
@@ -1,42 +1,33 @@
 <?php
-
-/* Endpoint */
-//headers
+// required headers
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
+header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-//include Object files and DB
-
+ 
+ 
 include_once '../config/database.php';
 include_once '../objects/project.php';
-
+ 
 $database = new Database();
 $db = $database->getConnection();
-
+ 
 $project = new Project($db);
-
+ 
 $data = json_decode(file_get_contents("php://input"));
 
 $project->id = $data->id;
-
-$project->address = $data->address;
-$project->title = $data->title;
-$project->description = $data->description;
-$project->open_date = $data->open_date;
-$project->tags = $data->tags;
-$project->progress = $data->progress;
-$project->hits = $data->hits;
-$project->img = $data->img;
-
-if ($project->update()) {
+ 
+if($project->delete()){
     echo '{';
-    echo 'Hell did not break loose';
-    echo '}';
-} else {
-    echo '{';
-    echo 'Hell broke loose';
+        echo '"message": "Product was deleted."';
     echo '}';
 }
+ 
+else{
+    echo '{';
+        echo '"message": "Unable to delete object."';
+    echo '}';
+}
+?>
